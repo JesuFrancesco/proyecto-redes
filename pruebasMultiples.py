c6382a834cd3e14bc458b3da2e4bf3014b87939c
jesu <@>
2023-11-06 13:55:06 -0500

    mejora de gui

--- pruebasMultiples.py
+++ pruebasMultiples.py
@@ -5,22 +5,24 @@ from speedtest import Speedtest
 from ping3 import ping
 import subprocess
 
-def pruebaLatencia(ip:str):
+def pruebaLatencia(ip:str, region:str):
     latencia = ping(dest_addr=ip, unit="ms")
-    print(f"Latencia: {latencia} ms")
-    # return latencia
+    # print(f"Latencia: {latencia} ms")
+    return latencia
 
-def pruebaST(ip:str):
+def pruebaST(ip:str, region:str):
     # sp_test = Speedtest(source_address=ip)
     sp_test = Speedtest()
     velDescarga = sp_test.download(threads=None)/(1024*1024) # Obtener velocidad de descarga
     velSubida = sp_test.upload(threads=None)/(1024*1024) # Obtener velocidad de subida
-    print("Velocidad de descarga: %.2lf MBps\nVelocidad de subida: %.2lf MBps" % (velDescarga, velSubida))
+    # print("Velocidad de descarga: %.2lf MBps\nVelocidad de subida: %.2lf MBps" % (velDescarga, velSubida))
     # pprint(sp_test.results.dict())
-    # return (velDescarga,velSubida)
+    return (velDescarga,velSubida)
 
-def pruebaPPaquetes(ip:str):
+def pruebaPPaquetes(ip:str, region:str):
     cmd_ping = subprocess.run(["ping", "-n", "5", ip], stdout=subprocess.PIPE, text=True)
     # pprint(cmd_ping.stdout)
     paq_loss = cmd_ping.stdout.splitlines()[-3].split(",")[2].strip()
-    print(f"Loss: {paq_loss}")
+    return paq_loss
+    # print(f"Loss: {paq_loss}")
+    
--- ventana.py
+++ ventana.py
@@ -1,29 +1,128 @@
-from tkinter import*
+import tkinter
+from tkinter import ttk
+from tkinter import messagebox
+from time import sleep
 from pruebasMultiples import*
-import threading as th
-ventana = Tk()
-ventana.geometry("400x400")
-Label(text="Proyecto Redes PC", font=("Times", 20, "bold")).pack()
-ip_sv = "8.8.8.8"
-Button(text="Latencia", 
-       command=lambda: th.Thread(target=lambda: pruebaLatencia(ip=ip_sv)).start()
-       ).pack()
 
-Button(text="Velocidad", 
-       command=lambda: th.Thread(target=lambda: pruebaST(ip=ip_sv)).start()
-       ).pack()
-    
-Button(text="Perdida paquetes", 
-       command=lambda: th.Thread(target=lambda: pruebaPPaquetes(ip=ip_sv)).start()
-       ).pack()
+def capturarDatos():
+    ip = ip_textbox.get()
+    region = region_opciones.get()
+    if ip != "" and region != "":
+        for i in [AnchoB_result, Latencia_result, PerdidaP_result]:
+            i.config(text="Cargando...")
 
-op = StringVar()
-op.set("*")
-region = op.get()
-OptionMenu(ventana, op, *["*", "Norteamerica", "Sudamerica", "Europa", "Asia", "Oceania"]).pack()
+        rAnchoB = pruebaST(ip, region)
+        AnchoB_result.config(text=f"Descarga: {str(round(rAnchoB[0], 2))} MBps\nSubida: {str(round(rAnchoB[1], 2))} MBps")
+        Latencia_result.config(text=f"{str(round(pruebaLatencia(ip, region), 4))} msec")
+        PerdidaP_result.config(text=f"{pruebaPPaquetes(ip, region)} paquetes perdidos")
+    else:
+        tkinter.messagebox.showwarning("Error", "Complete los campos requeridos")
 
-Button(text="Exportar resultados", 
-       command=lambda: print("TODO")
-       ).pack()
+def hiloCD():
+    sleep(1)
+    import threading as t
+    x = t.Thread(target=lambda: capturarDatos())
+    x.start()
 
-ventana.mainloop()
+window = tkinter.Tk()
+window.title("PROYECTO FINAL: REDES DE COMPUTADORAS")
+
+
+frame = tkinter.Frame(window, bg="#FFFFFF")
+frame.pack()
+
+
+title_frame = tkinter.LabelFrame(frame, bg="#FFFFFF")
+title_frame.grid(row = 0, column = 0, sticky="news", padx= 12, pady=(12,2))
+
+
+title_label = tkinter.Label(title_frame, text = "  Analisis de la calidad \n de internet", font=("Courier New", 20, "bold"), bg="#FFFFFF", fg="#333333")
+title_label.grid(row = 0, column = 0 , padx = 21, pady= (10,2))
+
+
+img = tkinter.PhotoImage(file=r"ulimalogo.gif")
+
+from PIL import Image, ImageTk
+imagen_gif_1 = Image.open("ulimalogo.gif")
+frames1 = []
+try:
+    while True:
+        frames1.append(imagen_gif_1.copy())
+        imagen_gif_1.seek(len(frames1))
+except EOFError:
+    pass
+
+imagen = tkinter.Label(title_frame, image = img, bd=0)
+imagen.grid(row=1, column=0, pady= 15)
+
+def mostrar_siguiente_frame_GIF1(frame_index):
+    frame_actual = frames1[frame_index]
+    imagen1 = ImageTk.PhotoImage(frame_actual)
+    imagen.config(image=imagen1)
+    imagen.grid(row=3, column=0, sticky="WE")
+    imagen.image = imagen1  # Evitar que la imagen sea eliminada por el recolector de basura
+    window.after(50, mostrar_siguiente_frame_GIF1, (frame_index + 1) % len(frames1)) # 
+mostrar_siguiente_frame_GIF1(0)
+
+
+
+input_frame = tkinter.LabelFrame(frame, text = "  Ingreso de datos  ", bg="#FFFFFF")
+input_frame.grid(row = 1, column = 0, sticky="news", padx= 12, pady=2)
+
+
+ip_label = tkinter.Label(input_frame, text="IP:", bg="#FFFFFF")
+ip_label.grid(row=0,column=0)
+
+
+ip_textbox = tkinter.Entry(input_frame, width=18, bg="#FFFFFF")
+ip_textbox.grid(row=0, column=1)
+
+
+relleno = tkinter.Label(input_frame, text="             ", bg="#FFFFFF")
+relleno.grid(row=0, column=2)
+
+
+region_label = tkinter.Label(input_frame, text="Region:", bg="#FFFFFF")
+region_label.grid(row=0, column=3)
+
+
+region_opciones = ttk.Combobox(input_frame, values=["","Asia","America","Africa","Europa","Oceanía"], width=10, state='readonly')
+region_opciones.grid(row=0, column=4)
+
+
+boton_enviar = tkinter.Button(input_frame, text="          Enviar          ", command=hiloCD)
+boton_enviar.grid(row=2, column=2)
+
+
+for widget in input_frame.winfo_children():
+    widget.grid_configure(padx = 10, pady= 7.5)
+                         
+output_frame = tkinter.LabelFrame(frame, text = "  Resultados  ", bg="#FFFFFF")
+output_frame.grid(row = 2, column = 0, sticky="news", padx= 12, pady=(2,12))
+
+
+AnchoB_label = tkinter.Label(output_frame, text="Ancho de banda:", bg="#FFFFFF")
+AnchoB_label.grid(row=0,column=0)
+
+
+Latencia_label = tkinter.Label(output_frame, text="Latencia:", bg="#FFFFFF")
+Latencia_label.grid(row=0,column=1)
+
+
+PerdidaP_label = tkinter.Label(output_frame, text="Perdida de paquetes:", bg="#FFFFFF")
+PerdidaP_label.grid(row=0,column=2)
+
+
+for widget in output_frame.winfo_children():
+    widget.grid_configure(padx = 32.5, pady= 5)
+
+
+AnchoB_result = tkinter.Label(output_frame, text=" ", bg="#FFFFFF")
+AnchoB_result.grid(row=1,column=0, pady= (0,10))
+Latencia_result = tkinter.Label(output_frame, text=" ", bg="#FFFFFF")
+Latencia_result.grid(row=1,column=1, pady= (0,10))
+PerdidaP_result = tkinter.Label(output_frame, text=" ", bg="#FFFFFF")
+PerdidaP_result.grid(row=1, column=2, pady= (0,10))
+
+
+window.mainloop()
